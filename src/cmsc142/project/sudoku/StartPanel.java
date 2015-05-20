88fe3eb3a320134eb6f07ea0f523653ecaabfd3d
annemurielg <annemurielg@gmail.com>
2015-05-21 02:51:23 +0800

    panel

--- src/cmsc142/project/sudoku/StartPanel.java
+++ src/cmsc142/project/sudoku/StartPanel.java
@@ -3,9 +3,16 @@ package cmsc142.project.sudoku;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -15,7 +22,7 @@ public class StartPanel extends JPanel implements ActionListener{
 	JButton newGame;
 	JButton highScore;
 	JButton exit;
-	
+	private BufferedImage image;
 	public StartPanel(){
 		this.setPreferredSize(new Dimension(600,600));
 		this.setComponents();
@@ -24,14 +31,26 @@ public class StartPanel extends JPanel implements ActionListener{
 		this.setLayout(new FlowLayout(FlowLayout.CENTER, 400, 40));
 	}
 	
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
+		try {
+			image = ImageIO.read(new File("./resources/grid.jpg"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		g.drawImage(image, 0, 0, this);
+	}
+	
 	private void setComponents(){
-		sudoku = new JLabel("Sudoku");
+		sudoku = new JLabel(new ImageIcon("./resources/sudoku.png"));
 		newGame = new JButton("New Game");
 		highScore = new JButton("High Score");
 		exit = new JButton("Exit");
 		
 		sudoku.setFont(new Font("Serif", Font.BOLD, 100));
-		sudoku.setPreferredSize(new Dimension(400,250));
+		sudoku.setPreferredSize(new Dimension(500,250));
+		sudoku.setBounds(0, 0, 500, 250);
 		newGame.setPreferredSize(new Dimension(200,50));
 		highScore.setPreferredSize(new Dimension(200,50));
 		exit.setPreferredSize(new Dimension(200,50));
