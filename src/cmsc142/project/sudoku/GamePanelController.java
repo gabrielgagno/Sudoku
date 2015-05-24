c63b5e8e03b7985f9a907e57d06fc3feff9ff402
Julius Iglesia <juliusiglesia@gmail.com>
2015-05-24 21:43:54 +0900

    Refactor

--- src/cmsc142/project/sudoku/GamePanelController.java
+++ src/cmsc142/project/sudoku/GamePanelController.java
@@ -29,7 +29,7 @@ public class GamePanelController implements ActionListener, KeyListener{
 	private SudokuBoard currentBoard;
 	private HashSet<Point> errorCells;
 	int currentPuzzle=0;
-	int currentSolution = 0;
+	int currentSolutionPointer = 0;
 	List<int[][]> solution;
 	String currentType;
 	int tickCount = 0;
@@ -60,8 +60,8 @@ public class GamePanelController implements ActionListener, KeyListener{
 	@Override
 	public void actionPerformed(ActionEvent event) {
 		if(event.getSource().equals(gamePanel.getSolverButton())){
-			String[] optionLabels = { "Solve the current state.", "Solve the original state.", "Let me solve the puzzle!"};    
-			int response = JOptionPane.showOptionDialog(gamePanel, "Do you want to solve the puzzle using the current state or the original state?",  "Solve Puzzle Confirmation",  JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, optionLabels, optionLabels[0]);
+			String[] options = { "Solve the current state.", "Solve the original state."};    
+			int response = JOptionPane.showOptionDialog(gamePanel, "Do you want to solve the puzzle using the current state or the original state?",  "Solve Puzzle Confirmation",  JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
 			
 			boolean xSudoku = false;
 			boolean ySudoku = false;
@@ -90,7 +90,9 @@ public class GamePanelController implements ActionListener, KeyListener{
 					board = new SudokuBoard(currentBoard.getPuzzleSize(), currentStateOfTable);
 					SudokuUtils.solveUsingBacktracking(board, xSudoku, ySudoku);
 				}
-				
+				 
+				// If no solution to the current board, notif user
+			
 				solution =  null;
 				if(xSudoku){
 					if(ySudoku){
@@ -106,29 +108,29 @@ public class GamePanelController implements ActionListener, KeyListener{
 					}
 				}				
 				
-				currentSolution = 0;
-				drawTable(solution.get(currentSolution));
+				currentSolutionPointer = 0;
+				drawTable(solution.get(currentSolutionPointer));
 				if(solution.size() > 1)
 					gamePanel.getNextSolutionButton().setEnabled(true);
 				gamePanel.getTimer().stop();
 			}
 			
-		} else if(event.getSource() == gamePanel.getNextSolutionButton()){
-			currentSolution++;
-			drawTable(solution.get(currentSolution));
-			if(currentSolution+1 == solution.size())
+		} else if(event.getSource().equals(gamePanel.getNextSolutionButton())){
+			currentSolutionPointer++;
+			drawTable(solution.get(currentSolutionPointer));
+			if(currentSolutionPointer+1 == solution.size())
 				gamePanel.getNextSolutionButton().setEnabled(false);
 			gamePanel.getPrevSolutionButton().setEnabled(true);
 				
-		} else if(event.getSource() == gamePanel.getNextPuzzleButton()){
-			int response = JOptionPane.showConfirmDialog(gamePanel, (Object)new JLabel("Previous changes will not be saved. Do you really want to proceed to the next puzzle?"), "Warning!", JOptionPane.OK_CANCEL_OPTION);
+		} else if(event.getSource().equals(gamePanel.getNextPuzzleButton())){
+			int response = JOptionPane.showConfirmDialog(gamePanel, new JLabel("Previous changes will not be saved. Do you really want to proceed to the next puzzle?"), "Warning!", JOptionPane.OK_CANCEL_OPTION);
 			if(response == JOptionPane.OK_OPTION){
 				currentPuzzle++;
 				currentBoard = sudokuBoards.get(currentPuzzle);
 				drawTable(currentBoard.getPuzzle());
 				tickCount = 0;
 				
-				this.errorCells = new HashSet<>();
+				errorCells.clear();
 				currentType = gamePanel.getTypeComboBox().getItemAt(0).toString();
 				gamePanel.getTypeComboBox().setSelectedItem(currentType);
 
@@ -136,14 +138,14 @@ public class GamePanelController implements ActionListener, KeyListener{
 				if(currentPuzzle > 0) gamePanel.getPrevPuzzleButton().setEnabled(true);
 			}
 		} else if(event.getSource() == gamePanel.getPrevPuzzleButton()){	
-			int response = JOptionPane.showConfirmDialog(gamePanel, (Object)new JLabel("Previous changes will not be saved. Do you really want to proceed to the previous puzzle?"), "Warning!", JOptionPane.OK_CANCEL_OPTION);
+			int response = JOptionPane.showConfirmDialog(gamePanel, new JLabel("Previous changes will not be saved. Do you really want to proceed to the previous puzzle?"), "Warning!", JOptionPane.OK_CANCEL_OPTION);
 			if(response == JOptionPane.OK_OPTION){
 				currentPuzzle--;
 				currentBoard = sudokuBoards.get(currentPuzzle);
 				drawTable(currentBoard.getPuzzle());
 				tickCount = 0;
 				
-				this.errorCells = new HashSet<>(); 
+				errorCells.clear();
 				currentType = gamePanel.getTypeComboBox().getItemAt(0).toString();
 				gamePanel.getTypeComboBox().setSelectedItem(currentType);
 				
@@ -191,7 +193,7 @@ public class GamePanelController implements ActionListener, KeyListener{
 			}
 		
 			if(!gamePanel.getTypeComboBox().getSelectedItem().toString().equals(currentType)){
-				int response = JOptionPane.showConfirmDialog(gamePanel, (Object)new JLabel("Previous changes will not be saved. Do you really want to change the puzzle type?"), "Warning!", JOptionPane.OK_CANCEL_OPTION);
+				int response = JOptionPane.showConfirmDialog(gamePanel, new JLabel("Previous changes will not be saved. Do you really want to change the puzzle type?"), "Warning!", JOptionPane.OK_CANCEL_OPTION);
 				if(response == JOptionPane.OK_OPTION){
 					currentType = (String) gamePanel.getTypeComboBox().getSelectedItem();
 
@@ -316,27 +318,27 @@ public class GamePanelController implements ActionListener, KeyListener{
 			}
 			
 			if(currentType.equals("Normal")){
-				errorCells = SudokuUtils.checkPuzzle(currentBoard, false, false, true);
+				errorCells = SudokuUtils.checkPuzzle(currentBoard, false, false, false);
 			} else if(currentType.equals("X")){
-				errorCells = SudokuUtils.checkPuzzle(currentBoard, true, false, true);
+				errorCells = SudokuUtils.checkPuzzle(currentBoard, true, false, false);
 			} else if(currentType.equals("Y")){
-				errorCells = SudokuUtils.checkPuzzle(currentBoard, false, true, true);
+				errorCells = SudokuUtils.checkPuzzle(currentBoard, false, true, false);
 			} else if(currentType.equals("XY")){
-				errorCells = SudokuUtils.checkPuzzle(currentBoard, true, true, true);
+				errorCells = SudokuUtils.checkPuzzle(currentBoard, true, true, false);
 			}
 			
 			if(errorCells.isEmpty()){
 				int [][] checkComplete = currentStateOfTable;
-				boolean flag = true;
+				boolean areInputsComplete = true;
 				
 				for (int i = 0; i < checkComplete.length; i++) {
 					for (int j = 0; j < checkComplete[i].length; j++) {
 						if (checkComplete[i][j] == 0)
-							flag = false;
+							areInputsComplete = false;
 					}
 				}
 				
-				if(flag){
+				if(areInputsComplete){
 					String[] options = {"OK"};
 					JPanel panel = new JPanel();
 					JLabel label = new JLabel("Enter Your name : ");
